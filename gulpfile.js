e8042c80b6a3bba370c64ba3a97b353c661aa6de
Kenneth Korcal <kakorcal@gmail.com>
2017-10-22 21:18:58 -0700

    adding less to less folder

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require('gulp');
+var scss = require('gulp-scss');
+var autoprefixer = require('gulp-autoprefixer');
+var watch = require('gulp-watch');
+
+// bundle scss and js files into one file and move to assets folder
