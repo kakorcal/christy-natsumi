99f01f6bb00ca0aca59b219fa4ece39fe8e8b173
Kenneth Korcal <kakorcal@gmail.com>
2017-10-28 00:22:09 -0700

    adding gulp task to concatenate scss files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,31 @@
 var gulp = require('gulp');
-var scss = require('gulp-scss');
-var autoprefixer = require('gulp-autoprefixer');
-var watch = require('gulp-watch');
+var gss = require('gulp-shopify-sass');
+var clean = require('gulp-clean');
+var rename = require('gulp-rename');
+// var sass = require('gulp-sass');
+// var sassGlob = require('gulp-sass-glob');
+// var watch = require('gulp-watch');
 
 // bundle scss and js files into one file and move to assets folder
+
+// gulp.task('scss-bundle', function() {
+//   return gulp.src('lib/styles/scss/main.scss')
+//     .pipe(sassGlob())
+//     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+//     .pipe(rename('main.scss.liquid'))
+//     .pipe(gulp.dest('assets'));
+// });
+
+gulp.task('scss-bundle', function() {
+  return gulp.src('lib/styles/scss/main.scss')
+    .pipe(gss())
+    .pipe(rename('main.scss.liquid'))
+    .pipe(gulp.dest('assets'));
+});
+
+gulp.task('scss-clean', function() {
+  return gulp.src('assets/main.scss.liquid', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('scss', ['scss-clean', 'scss-bundle']);
